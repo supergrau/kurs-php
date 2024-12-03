367e3ad85a0c6a346151f1171e289b3b2f7c3fdd
micha <mile@lincont.de>
2024-12-03 09:06:01 +0100

    Start.php

---
+++ 02-grundlegenede_sprachelemente/start.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../styles/simple.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <h1>Astrofotografie</h1>
+
+        <nav>
+            <a href="start.php">Startseite</a>
+            <a href="m31.php">M 31</a>
+            <a href="ngc654.php">NGC 654 & 663</a>
+            <a href="sh158.php">Sh 158</a>
+        </nav>
+    </header>
+
+    <main>
+        <h2>Astrofotos aus dem Garten</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora voluptas nihil animi earum voluptatem vel
+            beatae nostrum, quas sed rerum amet reiciendis, quo quisquam consectetur ut a quia, ad temporibus!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis distinctio quod delectus unde velit
+            molestiae culpa blanditiis, doloremque quam nostrum quas sed quasi consequatur? Blanditiis unde cum qui
+            voluptates delectus?</p>
+    </main>
+
+    <footer>
+        <p>Ein erstes Projekt im PHP-Kurs</p>
+    </footer>
+</body>
+
+</html>
