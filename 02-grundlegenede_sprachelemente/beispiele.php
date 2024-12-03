780af57e18251192d521be29f83f5c97a77c4b39
micha <mile@lincont.de>
2024-12-03 08:55:51 +0100

    Thema Grundlagen

---
+++ 02-grundlegenede_sprachelemente/beispiele.php
@@ -0,0 +1,16 @@
+<?php
+// Das ist ein einzeiliger Kommentar
+/**
+ * Ein mehrzeiliger Kommentar
+ *  
+ * 
+ */
+
+ # Auch ein einzeiliger Kommentar
+
+ $i = 4.5;
+
+ $txt = "Beispieltext: <b>\"Hallo\"</b>";
+
+ echo $txt;
+?>
---
+++ 02-grundlegenede_sprachelemente/seiteninhalt.php
@@ -0,0 +1,7 @@
+<h1>Seiteninhalt...</h1>
+<b>
+    <?php
+        echo "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae quam est reprehenderit nobis eos necessitatibus! Perspiciatis fugit ratione cupiditate blanditiis cumque nobis dignissimos ipsam iusto quis, numquam ducimus vel recusandae?";
+        
+    ?>
+</b>
