d66d0873477a581e487eeaabb7d7388888c725d2
micha <mile@lincont.de>
2024-12-09 13:51:31 +0100

    Upload

---
+++ 08-dateien/fileupload.php
@@ -0,0 +1,36 @@
+<?php
+require_once "../inc/htmlhelper.php";
+
+htmlanfang("File-Upload", "File-Upload");
+
+if (isset($_FILES["Datei"])) {
+
+    ksort($_FILES["Datei"]);
+    reset($_FILES["Datei"]);
+
+    echo "<table>";
+    foreach ($_FILES["Datei"] as $schluessel => $wert) {
+        $wert = is_string($wert) ? htmlspecialchars($wert) : "";
+        echo "<tr><td>$schluessel</td><td>$wert</td></tr>";
+    }
+    echo "</table>";
+
+    if (is_uploaded_file($_FILES['Datei']['tmp_name'])) {
+        echo "Datei " . $_FILES['Datei']['name'] . " erfolgreich herauf geladen.\n";
+        $tmp_name = $_FILES['Datei']['tmp_name'];
+        $name = $_FILES['Datei']['name'];
+        move_uploaded_file($tmp_name, "./$name");
+    } else {
+        echo "Mögliche Dateiupload-Attacke: ";
+        echo "Dateiname '" . $_FILES['Datei']['tmp_name'] . "'.";
+    }
+}
+?>
+<form method="post" enctype="multipart/form-data" action="">
+    <input type="file" name="Datei" />
+    <input type="submit" value="Upload" />
+</form>
+
+<?php
+htmlende();
+?>
