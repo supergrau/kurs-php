dd0a3c75b509c6e77a105dd00b8daa642856258f
micha <mile@lincont.de>
2024-12-11 14:18:19 +0100

    Webshop 3

--- projekte/webshop/inc/web_shop.inc.php
+++ projekte/webshop/inc/web_shop.inc.php
@@ -1,10 +1,9 @@
 <?php
-   /* Abteilungen */
-   $sql = "SELECT * FROM abteilung";
-   $res = $con->execute_query($sql);
-   echo "<p>Abteilungen:<br>";
-   while($dsatz = $res->fetch_assoc())
-      echo $dsatz["abteilung_id"] . ": <a href='web_shop.php?abt="
-         . $dsatz["abteilung_id"] . "'>" . $dsatz["abteilung_name"] . "</a><br>";
-   $res->close();
-?>
+/* Abteilungen */
+$sql = "SELECT * FROM abteilung";
+$res = $con->execute_query($sql);
+echo "<p>Abteilungen:<br>";
+while ($dsatz = $res->fetch_assoc())
+    echo $dsatz["abteilung_id"] . ": <a href='web_shop.php?abt="
+        . $dsatz["abteilung_id"] . "'>" . $dsatz["abteilung_name"] . "</a><br>";
+$res->close();
